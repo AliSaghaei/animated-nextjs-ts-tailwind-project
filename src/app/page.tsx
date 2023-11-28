e69938a1e8b26ff700f4f7d048a443407d820d13
AliSaghaei <79021514+AliSaghaei@users.noreply.github.com>
2023-11-28 08:02:08 -0800

    countdown and framework rotation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 import Image from 'next/image';
+import { Poppins } from 'next/font/google';
 import { assets } from '@/utils/asset-utils';
 import { type Frameworks, frameworks } from '@/utils/framework-utils';
 import { useState, useEffect } from 'react';
 import { cn } from '@/utils/tailwind-utils';
+import { FrameworkRotation } from '@/components/framework-rotation';
+import { CountdownTime } from '@/components/countdown-timer';
+
+const poppins = Poppins({
+  weight: '700',
+  subsets: ['latin'],
+});
 
 export default function Home() {
   const [currentFramework, setCurrentFramework] = useState<Frameworks>(frameworks[0]);
+  const [showBackground, setShowBackground] = useState(false);
 
   useEffect(() => {
     let currentIndex = 0;
@@ -18,6 +27,10 @@ export default function Home() {
 
     const intervalId = setInterval(rotateFrameworks, 2000);
     return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    setShowBackground(true);
   }, []);
 
   return (
@@ -44,6 +57,55 @@ export default function Home() {
         src={assets.gradient}
       />
       <div className='fixed inset-0 opacity-30' style={{ backgroundImage: `url(${assets.square})`, backgroundSize: '30px' }} />
+      <div className={cn('bg-black fixed inset-0 transition-opacity duration-[1500ms]', !showBackground ? 'opacity-100' : 'opacity-0')} />
+
+      <div className='max-w-7xl mt-20 mx-auto'>
+        <div className='flex flex-col items-center relative z-10'>
+          <h1 className={`text-5xl max-w-3xl text-center leading-snug mb-12 ${poppins.className}`}>
+            <Image alt='Figma logo' className='inline-block mr-8 -mt-2' src={assets.figma} width='50' height='50' />
+            to <FrameworkRotation currentFramework={currentFramework} /> will{' '}
+            <span
+              className={cn('transition-color duration-200', {
+                'text-purple-300': currentFramework === 'qwik',
+                'text-sky-300': currentFramework === 'safari',
+                'text-yellow-300': currentFramework === 'chrome',
+                'text-teal-300': currentFramework === 'tailwind',
+                'text-blue-300': currentFramework === 'react',
+                'text-green-300': currentFramework === 'vue',
+                'text-orange-400': currentFramework === 'svelte',
+                'text-red-300': currentFramework === 'mobile',
+                'text-neutral-300': currentFramework === 'desktop',
+              })}>
+              never
+            </span>{' '}
+            be the same again
+          </h1>
+
+          <p className='mb-8'>
+            <span className='text-gray-300'>Join us for an AI launch event by </span>
+            <Image alt='Builder.io Logo' className='inline-block ml-1 -mt-1' height='20' width='100' src={assets.builder} />
+            {' + '}
+            <Image alt='Figma Logo' className='inline-block mx-1' height='20' width='55' src={assets.figmatwo} />
+          </p>
+          <div className='mb-8'>
+            <button
+              className={cn('text-black px-6 py-3 rounded-md text-sm font-semibold transition-color duration-200', {
+                'bg-purple-300': currentFramework === 'qwik',
+                'bg-sky-300': currentFramework === 'safari',
+                'bg-yellow-300': currentFramework === 'chrome',
+                'bg-teal-300': currentFramework === 'tailwind',
+                'bg-blue-300': currentFramework === 'react',
+                'bg-green-300': currentFramework === 'vue',
+                'bg-orange-400': currentFramework === 'svelte',
+                'bg-red-300': currentFramework === 'mobile',
+                'bg-neutral-300': currentFramework === 'desktop',
+              })}>
+              Claim Tickets
+            </button>
+          </div>
+          <CountdownTime currentFramework={currentFramework} />
+        </div>
+      </div>
     </main>
   );
 }
